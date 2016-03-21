3fe3b2e92426c0f47c9e2be9651133ac60e87e83
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2016-03-21 13:47:19 +0100

    Login behörighet

--- LMSProj2HRL/Helpers/IsValid.cs
+++ LMSProj2HRL/Helpers/IsValid.cs
@@ -19,33 +19,52 @@ namespace LMSProj2HRL.Helpers
         public static bool CheckIsValid(string _loginId, string _passWD)
         {
             using (var cn = new SqlConnection(@"Data Source=(LocalDb)\v11.0;AttachDbFilename=|DataDirectory|\aspnet-LMSProj2HRL-20160316022928.mdf;Initial Catalog=aspnet-LMSProj2HRL-20160316022928;Integrated Security=True"))
-
-                {
-                    string _sql = @"SELECT [LoginId] FROM [dbo].[Teachers] " +
-                       @"WHERE [LoginId] = @u AND [PassWD] = @p";
-                var cmd = new SqlCommand(_sql, cn);
-                cmd.Parameters
+            {
+                string _sql1 = @"SELECT LoginId FROM Teachers WHERE LoginId = @u AND PassWD = @p";
+                var cmd1 = new SqlCommand(_sql1, cn);
+                cmd1.Parameters
                     .Add(new SqlParameter("@u", SqlDbType.NVarChar))
                     .Value = _loginId;
-                cmd.Parameters
+                cmd1.Parameters
                     .Add(new SqlParameter("@p", SqlDbType.NVarChar))
-                    .Value = _passWD;
-                    //.Value = Sha1.Encode(_passWD);
+                    .Value = Sha1.Encode(_passWD);
                 cn.Open();
-                var reader = cmd.ExecuteReader();
-                if (reader.HasRows)
+                var reader1 = cmd1.ExecuteReader();
+                if (reader1.HasRows)
                 {
-                    reader.Dispose();
-                    cmd.Dispose();
+                    // Sätt global variabel till Teachers status
+                    reader1.Dispose();
+                    cmd1.Dispose();
                     return true;
                 }
                 else
                 {
-                    reader.Dispose();
-                    cmd.Dispose();
-                    return false;
+                    cn.Close();
+                    string _sql2 = @"SELECT LoginId FROM Students WHERE LoginId = @u AND PassWD = @p";
+                    var cmd2 = new SqlCommand(_sql2, cn);
+                    cmd2.Parameters
+                        .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                        .Value = _loginId;
+                    cmd2.Parameters
+                        .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                        .Value = Sha1.Encode(_passWD);
+                    cn.Open();
+                    var reader2 = cmd2.ExecuteReader();
+                    if (reader2.HasRows)
+                    {
+                        // Sätt global variabel till Students status
+                        reader2.Dispose();
+                        cmd2.Dispose();
+                        return true;
+                    }
+                    else
+                    {
+                        reader2.Dispose();
+                        cmd2.Dispose();
+                        return false;
+                    }
                 }
             }
-        } 
+        }
     }
 }
