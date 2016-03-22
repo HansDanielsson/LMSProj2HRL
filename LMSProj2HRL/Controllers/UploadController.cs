54261608fb45e61a2ed64c71109dcab779b3bc11
Hans Danielsson <Hans.Ro.Danielsson@gmail.com>
2016-03-22 14:52:58 +0100

    Controller add?

---
+++ LMSProj2HRL/Controllers/UploadController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMSProj2HRL.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = "hej";
+            }
+            return View();
+        }
+    }
+}
